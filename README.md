5e036e5f9052fdb0caec9f06bd4d3ee0d00b7528
norabolivar <norabolivar@uoc.edu>
2025-10-20 21:52:44 +0200

    open station list by using selenium and Chrome

--- README.md
+++ README.md
@@ -9,11 +9,32 @@
 
 - `src/main.py`: punt d'entrada al programa. Inicia el procés de scraping.
 - `src/scraper.py`: conté la implementació de la classe MeteoScraper amb els mètodes que generen el conjunt de dades a partir de les dades recollides del llista d'estacions https://www.meteo.cat//observacions/llistat-xema.
+- `requirements.txt`: fitxer ams els requisits
 
 ## Utilització del codi
 
+### Creació d'un entorn virtual amb venv
+
 ```sh
-$ python3 src/main.py
+python3 -m venv .venv
+```
+
+### Activació de l'entorn virtual
+
+```sh
+source .venv/bin/activate
+```
+
+### Instal·lació dels requisits
+
+```sh
+python -m pip install -r requirements.txt
+```
+
+### Execució del codi
+
+```sh
+(.venv) $ python3 src/main.py
 Scraping data...
 Saving data to dataset.csv...
 ```
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,3 +1,7 @@
+import time
+from selenium import webdriver
+
+
 class MeteoScraper:
     def __init__(self):
         self.base_url = "https://www.meteo.cat"
@@ -6,8 +10,21 @@ class MeteoScraper:
 
     def scrape(self):
         # Implementation of the scrape method
-        print("Scraping data...")
-        pass
+        print("\tScraping data...")
+        try:
+            driver = webdriver.Chrome()
+            self.driver = webdriver.Chrome()
+            # Get and print user agent
+            agent = self.driver.execute_script("return navigator.userAgent")
+            print(f"User agent: {agent}")
+            # Navigate to the list URL
+            self.driver.get(self.list_url)
+            # wait five seconds
+            time.sleep(5)
+            # quit the driver
+            self.driver.quit()
+        except Exception as e:
+            print(f"Error occurred while scraping: {e}")
 
     def data2csv(self, filename):
         # Implementation of the data2csv method
