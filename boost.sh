a3cc949e3f8dc6f4eafe963a0e078896b4d40ed5
Robert Buj <robert.buj@gmail.com>
2025-12-05 17:25:24 +0100

    update boost.sh

--- boost.sh
+++ boost.sh
@@ -1,16 +1,23 @@
-#!/bin/bash
-## nombre maxim de processos en paral·lel
+#!/usr/bin/env bash
+
+# Altres shebangs possibles:
+##!/bin/bash
+##!/opt/homebrew/bin/bash
+##!/usr/local/bin/bash
+
+# nombre maxim de processos en paral·lel
 NPROCS=6
 
 for i in {1..365}
 do
    DAY=$(date -v-${i}d +"%d.%m.%Y")
    python3 source/main.py -d 1 -b ${DAY} -m > ${DAY}.log &
-   ## executa com a molt NPROCS processos a la vegada, utilitza wait -n per esperar que acabi un abans de llançar-ne un altre
+   # executa com a molt NPROCS processos a la vegada, utilitza wait -n per
+   # esperar que acabi un abans de llançar-ne un altre
    while [ $(jobs -r | wc -l) -ge ${NPROCS} ]; do
        wait -n
    done
-   ## Espera un moment abans de llançar el següent procés
+   # Espera un moment abans de llançar el següent procés
    sleep 1
 done
 
